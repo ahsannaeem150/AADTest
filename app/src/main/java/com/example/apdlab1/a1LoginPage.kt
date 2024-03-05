090fcdb5785e98514dd90b526ef555f84c0909f1
Ahsan Naeem <ahsannaeem150@gmail.com>
2024-03-05 09:59:47 +0500

    Complete login form page

--- app/src/main/java/com/example/apdlab1/a1LoginPage.kt
+++ app/src/main/java/com/example/apdlab1/a1LoginPage.kt
@@ -15,21 +15,29 @@ class a1LoginPage : AppCompatActivity() {
         var correctUsername = true
         val username = "ahsannaeem150"
         val password = "12345678"
-        val usernameTxt:EditText = findViewById(R.id.usernameTxt)
-        val passwordTxt:EditText = findViewById(R.id.passwordTxt)
-        val loginBtn:Button = findViewById(R.id.loginBtn)
+        val usernameTxt: EditText = findViewById(R.id.usernameTxt)
+        val passwordTxt: EditText = findViewById(R.id.passwordTxt)
+        val loginBtn: Button = findViewById(R.id.loginBtn)
 
-        loginBtn.setOnClickListener(){
-            if(usernameTxt.text.toString() != username){
+        loginBtn.setOnClickListener() {
+            if (usernameTxt.text.toString() != username) {
                 correctUsername = false;
             }
-            if(passwordTxt.text.toString() != password){
+            if (passwordTxt.text.toString() != password) {
                 correctPassword = false
             }
-            if(correctUsername && correctPassword){
-                Toast.makeText(this ,"Login Successful!" , Toast.LENGTH_SHORT ).show();
+            if ((usernameTxt.text.toString() == username) && (passwordTxt.text.toString() == password)) {
+                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                usernameTxt.setText("")
+                passwordTxt.setText("")
             }
-            
+            if(correctUsername && !(correctPassword)){
+                Toast.makeText(this , "Wrong Password!" , Toast.LENGTH_SHORT).show();
+            } else if(correctPassword && !(correctUsername)){
+                Toast.makeText(this , "Wrong Username!" , Toast.LENGTH_SHORT).show();
+            } else if(!correctPassword && !correctUsername){
+                Toast.makeText(this , "Wrong Credentials!" , Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
