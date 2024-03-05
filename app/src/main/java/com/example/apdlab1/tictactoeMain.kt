58f46891bf6aa23d0153346f1cdf85aefdf404e1
Ahsan Naeem <ahsannaeem150@gmail.com>
2024-03-06 01:41:17 +0500

    Whole logic working except checkResult()
    -fix logical operations in next build

--- app/src/main/java/com/example/apdlab1/tictactoeMain.kt
+++ app/src/main/java/com/example/apdlab1/tictactoeMain.kt
@@ -2,6 +2,7 @@ package com.example.apdlab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
@@ -15,27 +16,39 @@ class ticktactoeMain : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.ticktactoe)
         homeBinding = DataBindingUtil.setContentView(this , R.layout.ticktactoe)
-
-
+        homeBinding.endGame.setVisibility(View.GONE);           //hiding reset button until someone wins
+        homeBinding.player.setText("Player 1")
         var playerTurn: Boolean = true
-        fun switchTurn(){
-            playerTurn = !playerTurn
 
+        fun switchTurn(){
+            if(playerTurn){
+                homeBinding.player.setText("Player 2")
+            } else {
+                homeBinding.player.setText("Player 1")
+            }
+            playerTurn = !playerTurn
         }
+        
+        fun endGame(){
+            homeBinding.endGame.setVisibility(View.VISIBLE);
+        }
+
         fun currentPlayerWon(){
             if(playerTurn){
                 homeBinding.resultTxt.text = "Winner: Player 1"
             } else {
                 homeBinding.resultTxt.text = "Winner: Player 2"
             }
+            endGame()
         }
 
+
         fun checkResult(){
-            if((homeBinding.topLeftTxt.text.toString() == homeBinding.topCenterTxt.text.toString()) && (homeBinding.topLeftTxt.text.toString() == homeBinding.topRightTxt.text.toString())) {
+            if((homeBinding.topLeftTxt.text.toString() == homeBinding.topCenterTxt.text.toString()) && (homeBinding.topLeftTxt.text.toString() == homeBinding.topRightTxt.text.toString()) && (homeBinding.topLeftTxt.text.toString() == "O" || homeBinding.topLeftTxt.text.toString() == "1")) {
                 currentPlayerWon()
-            } else if((homeBinding.centerLeftTxt.text.toString() == homeBinding.centerTxt.text.toString()) && (homeBinding.centerLeftTxt.text.toString() == homeBinding.centerRightTxt.text.toString())) {
+            } else if((homeBinding.centerLeftTxt.text.toString() == homeBinding.centerTxt.text.toString()) && (homeBinding.centerLeftTxt.text.toString() == homeBinding.centerRightTxt.text.toString()) && (homeBinding.centerRightTxt.text.toString() == "O" || homeBinding.centerRightTxt.text.toString() == "1")) {
                 currentPlayerWon()
-            } else if((homeBinding.bottomLeftTxt.text.toString() == homeBinding.bottomCenterTxt.text.toString()) && (homeBinding.bottomLeftTxt.text.toString() == homeBinding.bottomRightTxt.text.toString())){
+            } else if((homeBinding.bottomLeftTxt.text.toString() == homeBinding.bottomCenterTxt.text.toString()) && (homeBinding.bottomLeftTxt.text.toString() == homeBinding.bottomRightTxt.text.toString())  && (homeBinding.bottomRightTxt.text.toString() == "O" || homeBinding.bottomRightTxt.text.toString() == "1")){
                 currentPlayerWon()
             }  else if((homeBinding.topLeftTxt.text.toString() == homeBinding.centerLeftTxt.text.toString()) && (homeBinding.topLeftTxt.text.toString() == homeBinding.bottomLeftTxt.text.toString())){
                 currentPlayerWon()
@@ -58,6 +71,7 @@ class ticktactoeMain : AppCompatActivity() {
                 } else {
                     clickedBox.text = "O"
                 }
+                checkResult()
                 switchTurn()
             }
         }
@@ -65,120 +79,50 @@ class ticktactoeMain : AppCompatActivity() {
         homeBinding.topLeftTxt.setOnClickListener{
             playerAction(homeBinding.topLeftTxt)
         }
-        homeBinding.topLeftTxt.setOnClickListener{
-            playerAction(homeBinding.topLeftTxt)
+        homeBinding.topCenterTxt.setOnClickListener{
+            playerAction(homeBinding.topCenterTxt)
         }
-        homeBinding.topLeftTxt.setOnClickListener{
-            playerAction(homeBinding.topLeftTxt)
+        homeBinding.topRightTxt.setOnClickListener{
+            playerAction(homeBinding.topRightTxt)
         }
-        homeBinding.topLeftTxt.setOnClickListener{
-            playerAction(homeBinding.topLeftTxt)
+        homeBinding.centerLeftTxt.setOnClickListener{
+            playerAction(homeBinding.centerLeftTxt)
         }
-        homeBinding.topLeftTxt.setOnClickListener{
-            playerAction(homeBinding.topLeftTxt)
+        homeBinding.centerTxt.setOnClickListener{
+            playerAction(homeBinding.centerTxt)
         }
-        homeBinding.topLeftTxt.setOnClickListener{
-            playerAction(homeBinding.topLeftTxt)
+        homeBinding.centerRightTxt.setOnClickListener{
+            playerAction(homeBinding.centerRightTxt)
         }
-        homeBinding.topLeftTxt.setOnClickListener{
-            playerAction(homeBinding.topLeftTxt)
+        homeBinding.bottomLeftTxt.setOnClickListener{
+            playerAction(homeBinding.bottomLeftTxt)
         }
-        homeBinding.topLeftTxt.setOnClickListener{
-            playerAction(homeBinding.topLeftTxt)
+        homeBinding.bottomCenterTxt.setOnClickListener{
+            playerAction(homeBinding.bottomCenterTxt)
         }
-        homeBinding.topLeftTxt.setOnClickListener{
-            playerAction(homeBinding.topLeftTxt)
+        homeBinding.bottomRightTxt.setOnClickListener{
+            playerAction(homeBinding.bottomRightTxt)
         }
-        homeBinding.topLeftTxt.setOnClickListener {
-            if(homeBinding.topLeftTxt.text.toString().isEmpty()){
-                if(playerTurn){
-                    homeBinding.topLeftTxt.text = "X"
-                } else {
-                    homeBinding.topLeftTxt.text = "O"
-                }
-                switchTurn()
-            }
+
+        fun newGame(){
+            homeBinding.topLeftTxt.text = ""
+            homeBinding.topCenterTxt.text = ""
+            homeBinding.topRightTxt.text = ""
+            homeBinding.centerLeftTxt.text = ""
+            homeBinding.centerTxt.text = ""
+            homeBinding.centerRightTxt.text = ""
+            homeBinding.bottomLeftTxt.text = ""
+            homeBinding.bottomCenterTxt.text = ""
+            homeBinding.bottomRightTxt.text = ""
+
+            homeBinding.resultTxt.text = ""
+            playerTurn = true
+            homeBinding.player.setText("Player 1")
+            homeBinding.endGame.setVisibility(View.GONE)
         }
-        homeBinding.topCenterTxt.setOnClickListener {
-            if(homeBinding.topCenterTxt.text.toString().isEmpty()){
-                if(playerTurn){
-                    homeBinding.topCenterTxt.text = "X"
-                } else {
-                    homeBinding.topCenterTxt.text = "O"
-                }
-                switchTurn()
-            }
-        }
-        homeBinding.topRightTxt.setOnClickListener {
-            if(homeBinding.topRightTxt.text.toString().isEmpty()){
-                if(playerTurn){
-                    homeBinding.topRightTxt.text = "X"
-                } else {
-                    homeBinding.topRightTxt.text = "O"
-                }
-                switchTurn()
-            }
-        }
-        homeBinding.centerLeftTxt.setOnClickListener {
-            if(homeBinding.centerLeftTxt.text.toString().isEmpty()){
-                if(playerTurn){
-                    homeBinding.centerLeftTxt.text = "X"
-                } else {
-                    homeBinding.centerLeftTxt.text = "O"
-                }
-                switchTurn()
-            }
-        }
-        homeBinding.centerTxt.setOnClickListener {
-            if(homeBinding.centerTxt.text.toString().isEmpty()){
-                if(playerTurn){
-                    homeBinding.centerTxt.text = "X"
-                } else {
-                    homeBinding.centerTxt.text = "O"
-                }
-                switchTurn()
-            }
-        }
-        homeBinding.centerRightTxt.setOnClickListener {
-            if(homeBinding.centerRightTxt.text.toString().isEmpty()){
-                if(playerTurn){
-                    homeBinding.centerRightTxt.text = "X"
-                } else {
-                    homeBinding.centerRightTxt.text = "O"
-                }
-                switchTurn()
-            }
-        }
-        homeBinding.bottomLeftTxt.setOnClickListener {
-            if(homeBinding.bottomLeftTxt.text.toString().isEmpty()){
-                if(playerTurn){
-                    homeBinding.bottomLeftTxt.text = "X"
-                } else {
-                    homeBinding.bottomLeftTxt.text = "O"
-                }
-                switchTurn()
-            }
-        }
-        homeBinding.bottomCenterTxt.setOnClickListener {
-            if(homeBinding.bottomCenterTxt.text.toString().isEmpty()){
-                if(playerTurn){
-                    homeBinding.bottomCenterTxt.text = "X"
-                } else {
-                    homeBinding.bottomCenterTxt.text = "O"
-                }
-                switchTurn()
-            }
-        }
-        homeBinding.bottomRightTxt.setOnClickListener {
-            if(homeBinding.bottomRightTxt.text.toString().isEmpty()){
-                if(playerTurn){
-                    homeBinding.bottomRightTxt.text = "X"
-                } else {
-                    homeBinding.bottomRightTxt.text = "O"
-                }
-                switchTurn()
-            }
+
+        homeBinding.endGame.setOnClickListener {
+            newGame()
         }
     }
-
 }
