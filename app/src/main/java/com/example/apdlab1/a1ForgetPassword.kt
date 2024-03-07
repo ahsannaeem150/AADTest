e8951b948a86888f9b5d779d6bdcb27728b4b7b7
Ahsan Naeem <ahsannaeem150@gmail.com>
2024-03-07 20:33:03 +0500

    Add forget password logic and front-end

---
+++ app/src/main/java/com/example/apdlab1/a1ForgetPassword.kt
@@ -0,0 +1,32 @@
+package com.example.apdlab1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.apdlab1.databinding.A1ForgetpasswordBinding
+
+class a1ForgetPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var dataBinding: A1ForgetpasswordBinding
+        super.onCreate(savedInstanceState)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.a1_forgetpassword)
+
+        var password: String = intent.extras?.getString("Password").toString();
+        dataBinding.confirmBtn.setOnClickListener() {
+            if (dataBinding.passwordTxt.text.toString() != dataBinding.confirmPasswordTxt.text.toString()) {
+                Toast.makeText(this, "Password doesn't match", Toast.LENGTH_SHORT).show()
+            } else {
+                if (dataBinding.passwordTxt.text.toString() == password) {
+                    Toast.makeText(this, "New Password Cannot Be Same As The Old Password", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Password Changed", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this , a1LoginPage::class.java)
+                    intent.putExtra("Password", dataBinding.confirmPasswordTxt.text.toString());
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/apdlab1/a1LoginPage.kt
+++ app/src/main/java/com/example/apdlab1/a1LoginPage.kt
@@ -1,9 +1,8 @@
 package com.example.apdlab1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.apdlab1.databinding.A1LoginpageBinding
@@ -15,8 +14,18 @@ class a1LoginPage : AppCompatActivity() {
         homeBinding = DataBindingUtil.setContentView(this , R.layout.a1_loginpage)
 
         val username = "ahsannaeem150"
-        val password = "12345678"
+        var password:String;
+        if(intent.extras != null){
+            password = intent.extras?.getString("Password").toString()
+        } else {
+            password = "12345678"
+        }
 
+        homeBinding.forgetPassword.setOnClickListener(){
+            val intent = Intent(this@a1LoginPage , a1ForgetPassword::class.java)
+            intent.putExtra("Password" , password)
+            startActivity(intent)
+        }
         homeBinding.loginBtn.setOnClickListener() {
             if ((homeBinding.usernameTxt.text.toString() == username) && (homeBinding.passwordTxt.text.toString() == password)) {
                 Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show();
@@ -24,15 +33,15 @@ class a1LoginPage : AppCompatActivity() {
                 homeBinding.passwordTxt.setText("")
             }
             if((homeBinding.usernameTxt.text.toString() == username) && (homeBinding.passwordTxt.text.toString() != password)){
-                Toast.makeText(this , "Wrong Password!" , Toast.LENGTH_SHORT).show();
+                Toast.makeText(this , password , Toast.LENGTH_SHORT).show();
             } else if((homeBinding.passwordTxt.text.toString() == password) && (homeBinding.usernameTxt.text.toString() != username)){
-                Toast.makeText(this , "Wrong Username!" , Toast.LENGTH_SHORT).show();
+                Toast.makeText(this , "Wrong Password" , Toast.LENGTH_SHORT).show();
             } else if((homeBinding.usernameTxt.text.toString() != username) && (homeBinding.passwordTxt.text.toString() != password)){
                 Toast.makeText(this , "Wrong Credentials!" , Toast.LENGTH_SHORT).show();
             }
         }
     }
-}
+}                                                                       
 
 
 
