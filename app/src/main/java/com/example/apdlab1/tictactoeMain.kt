ed62fda8986e0da9fceedbc7ef650cbbd3c3d3b1
Ahsan Naeem <ahsannaeem150@gmail.com>
2024-03-08 19:29:35 +0500

    Complete logic for drawing line on each win

--- app/src/main/java/com/example/apdlab1/tictactoeMain.kt
+++ app/src/main/java/com/example/apdlab1/tictactoeMain.kt
@@ -47,14 +47,31 @@ class ticktactoeMain : AppCompatActivity() {
             disableTurnsWhenSomeoneWins()
             endGame()
         }
+        fun drawLineOnCombination(componentOne: View ,componentTwo: View , componentThree: View, lineDegree: Float){
+            componentOne.visibility = View.VISIBLE
+            componentTwo.visibility = View.VISIBLE
+            componentThree.visibility = View.VISIBLE
+            componentOne.rotation = lineDegree
+            componentTwo.rotation = lineDegree
+            componentThree.rotation = lineDegree
+        }
         fun drawLine(combination: Int){
             if(combination == 1){
-                homeBinding.topLeftLine.visibility = View.VISIBLE
-                homeBinding.topCenterLine.visibility = View.VISIBLE
-                homeBinding.topRightLine.visibility = View.VISIBLE
-                homeBinding.topLeftLine.rotation = 90F
-                homeBinding.topCenterLine.rotation = 90F
-                homeBinding.topRightLine.rotation = 90F
+                drawLineOnCombination(homeBinding.topLeftLine , homeBinding.topCenterLine , homeBinding.topRightLine , 90F)
+            } else if(combination == 2){
+                drawLineOnCombination(homeBinding.middleLeftLine , homeBinding.centerLine , homeBinding.middleRightLine , 90F)
+            } else if(combination == 3){
+                drawLineOnCombination(homeBinding.bottomLeftLine , homeBinding.bottomCenterLine , homeBinding.bottomRightLine , 90F)
+            } else if(combination == 4){
+                drawLineOnCombination(homeBinding.topLeftLine , homeBinding.middleLeftLine , homeBinding.bottomLeftLine , 0F)
+            } else if(combination == 5){
+                drawLineOnCombination(homeBinding.topCenterLine , homeBinding.centerLine , homeBinding.bottomCenterLine , 0F)
+            } else if(combination == 6){
+                drawLineOnCombination(homeBinding.topRightLine, homeBinding.middleRightLine , homeBinding.bottomRightLine , 0F)
+            } else if(combination == 7){
+                drawLineOnCombination(homeBinding.topLeftLine, homeBinding.centerLine , homeBinding.bottomRightLine , 135F)
+            }  else if(combination == 8){
+                drawLineOnCombination(homeBinding.topRightLine, homeBinding.centerLine , homeBinding.bottomLeftLine , 45F)
             }
         }
         fun checkResult(){
@@ -63,18 +80,25 @@ class ticktactoeMain : AppCompatActivity() {
                 drawLine(1)
             } else if((homeBinding.middleLeftTxt.text.toString() == homeBinding.centerTxt.text.toString()) && (homeBinding.middleLeftTxt.text.toString() == homeBinding.middleRightTxt.text.toString()) && (homeBinding.centerTxt.text.toString().isNotEmpty())) {
                 currentPlayerWon()
+                drawLine(2)
             } else if((homeBinding.bottomLeftTxt.text.toString() == homeBinding.bottomCenterTxt.text.toString()) && (homeBinding.bottomLeftTxt.text.toString() == homeBinding.bottomRightTxt.text.toString())  && (homeBinding.bottomCenterTxt.text.toString().isNotEmpty())){
                 currentPlayerWon()
+                drawLine(3)
             }  else if((homeBinding.topLeftTxt.text.toString() == homeBinding.middleLeftTxt.text.toString()) && (homeBinding.topLeftTxt.text.toString() == homeBinding.bottomLeftTxt.text.toString()) && (homeBinding.middleLeftTxt.text.toString().isNotEmpty())){
                 currentPlayerWon()
+                drawLine(4)
             }   else if((homeBinding.topCenterTxt.text.toString() == homeBinding.centerTxt.text.toString()) && (homeBinding.topCenterTxt.text.toString() == homeBinding.bottomCenterTxt.text.toString())  && (homeBinding.centerTxt.text.toString().isNotEmpty()))  {
                 currentPlayerWon()
+                drawLine(5)
             }   else if((homeBinding.topRightTxt.text.toString() == homeBinding.middleRightTxt.text.toString()) && (homeBinding.topRightTxt.text.toString() == homeBinding.bottomRightTxt.text.toString())  && (homeBinding.middleRightTxt.text.toString().isNotEmpty())){
                 currentPlayerWon()
+                drawLine(6)
             }   else if((homeBinding.topLeftTxt.text.toString() == homeBinding.centerTxt.text.toString()) && (homeBinding.topLeftTxt.text.toString() == homeBinding.bottomRightTxt.text.toString()) && (homeBinding.bottomRightTxt.text.toString().isNotEmpty()))  {
                 currentPlayerWon()
+                drawLine(7)
             }   else if((homeBinding.topRightTxt.text.toString() == homeBinding.centerTxt.text.toString()) && (homeBinding.topRightTxt.text.toString() == homeBinding.bottomLeftTxt.text.toString()) && (homeBinding.bottomLeftTxt.text.toString().isNotEmpty())) {
                 currentPlayerWon()
+                drawLine(8)
             }
         }
 
